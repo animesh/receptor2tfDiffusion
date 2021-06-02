92414a0618ee2f1581d662d8519d97cf49cd6600
Amrinder Singh <aaarsh88@gmail.com>
2021-06-02 09:56:40 +0200

    Update diffusion.R

--- R/diffusion.R
+++ R/diffusion.R
@@ -113,7 +113,7 @@ diffusionMap <- function(receptors, TFs, M, network, nCores=2, nTicks=2000){
 
   testf <- function(receptor){
     # print(SignalOnNetwork)
-    aa <-   SignalOnNetwork(startingNet,outputNode = TFs,
+    aa <-   signalOnNetwork(startingNet,outputNode = TFs,
                                 inputNode = receptor, inputSignal = 0.99,n = nTicks)
     diff_time <- apply(aa[[1]],1,function(x){
       min(which(x > 0.5 * max(x)))
@@ -126,7 +126,7 @@ diffusionMap <- function(receptors, TFs, M, network, nCores=2, nTicks=2000){
   for(jj in colnames(M)){
     nodeW <- M[unique(c(network[,1],network[,2])),jj]
     startingNet <- initializeSignallingNetwork(network,nodeW)
-    diff_time <- foreach(receptor = receptors, .export=c('SignalOnNetwork','setkey'),.combine = rbind) %dopar% testf(receptor)
+    diff_time <- foreach(receptor = receptors, .export=c('signalOnNetwork','setkey'),.combine = rbind) %dopar% testf(receptor)
     rownames(diff_time) <- receptors
     diff_time[diff_time > nTicks] <- nTicks + 1
     sample_dtime[[jj]] <- diff_time
